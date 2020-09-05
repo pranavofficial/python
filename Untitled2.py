4463b55f43072cb05a976bc057f047fd280f8d89
pranavofficial <70779680+pranavofficial@users.noreply.github.com>
2020-09-05 10:33:33 -0700

    Update Untitled2.py

--- Untitled2.py
+++ Untitled2.py
@@ -3,17 +3,21 @@
 
 # In[ ]:
 
-
+#get letter they want checked
 letter = input("type a letter ")
+#get sentence to be checked
 sentence = input("type a sentence ")
-
+#set variable before loop
 count = 0
-
+#start loop
 for a in sentence:
+  #check if variable is as same as letter
   if (a == letter):
+    #add 1 to count
     count += 1
   else:
+    #dont increase count
    count += 0
-
+#display results
 print("There are "  + str(count) + " " + letter + "'s in " + sentence)
 
